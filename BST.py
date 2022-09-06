3e33bd375aa99b0ac6e85828def185d0e8a88ecd
haiderAtGitHub <haiderabbas1982000@gmail.com>
2022-09-06 23:37:57 +0500

    Added DFS traversals (in-order, pre-order, post-order)

--- BST.py
+++ BST.py
@@ -27,9 +27,45 @@ class BST:
         else:
             parentNode.left = Node(valueToInsert)
 
+    def recursiveInOrderTraversal(self):
+        print("Recursive In Order Traversal")
+
+        def inOrderTraversal(root):
+            if root:
+                inOrderTraversal(root.left)
+                print(root.value)
+                inOrderTraversal(root.right)
+
+        inOrderTraversal(self.root)
+
+    def recursivePreOrderTraversal(self):
+        print("Recursive Pre Order Traversal")
+
+        def preOrderTraversal(root):
+            if root:
+                print(root.value)
+                preOrderTraversal(root.left)
+                preOrderTraversal(root.right)
+
+        preOrderTraversal(self.root)
+
+    def recursivePostOrderTraversal(self):
+        print("Recursive Post Order Traversal")
+
+        def postOrderTraversal(root):
+            if root:
+                postOrderTraversal(root.left)
+                postOrderTraversal(root.right)
+                print(root.value)
+
+        postOrderTraversal(self.root)
+
 
 if __name__ == '__main__':
     binarySearchTree = BST()
     binarySearchTree.iterativeInsert(10)
     binarySearchTree.iterativeInsert(7)
     binarySearchTree.iterativeInsert(13)
+    binarySearchTree.recursiveInOrderTraversal()
+    binarySearchTree.recursivePreOrderTraversal()
+    binarySearchTree.recursivePostOrderTraversal()
