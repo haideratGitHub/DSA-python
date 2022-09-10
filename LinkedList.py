5bf923f3ca0932f40e4f54954174cb40ba0a859c
haiderAtGitHub <haiderabbas1982000@gmail.com>
2022-09-11 01:11:00 +0500

    Added linked list class

---
+++ LinkedList.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == '__main__':
+    List = LinkedList()
