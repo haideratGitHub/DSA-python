0e89e982e5a88bde40a49f9362e8f6d0523fa00e
haiderAtGitHub <haiderabbas1982000@gmail.com>
2022-09-11 01:18:44 +0500

    Added linked list iterative insert at end

--- LinkedList.py
+++ LinkedList.py
@@ -8,7 +8,17 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def iterativeInsertAtEnd(self, valueToInsert):
+        if self.head is None:
+            self.head = Node(valueToInsert)
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = Node(valueToInsert)
+
     def iterativePrint(self):
+        print("Linked list iterative print")
         current = self.head
         while current:
             print(current.value)
@@ -17,3 +27,6 @@ class LinkedList:
 
 if __name__ == '__main__':
     List = LinkedList()
+    List.iterativeInsertAtEnd(10)
+    List.iterativeInsertAtEnd(11)
+    List.iterativePrint()
