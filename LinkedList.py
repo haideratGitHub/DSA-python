62c9dd4796de4e351cfccdf9ee2074d1fb522d15
haiderAtGitHub <haiderabbas1982000@gmail.com>
2022-09-11 01:12:01 +0500

    Added linked list iterative print

--- LinkedList.py
+++ LinkedList.py
@@ -8,6 +8,12 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def iterativePrint(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+
 
 if __name__ == '__main__':
     List = LinkedList()
